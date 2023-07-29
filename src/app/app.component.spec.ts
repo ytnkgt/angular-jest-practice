a21b791729f95bb4205cbddca50ddb455d432447
ytnkgt <ytnkgt@gmail.com>
2023-07-29 11:55:53 +0900

    initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-jest-practice');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-jest-practice app is running!');
-  });
 });
